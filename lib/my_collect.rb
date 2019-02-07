a3654043185ac05d22bfb4743635f6bb38be3601
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-07 21:35:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,23 @@ def my_collect(a)
 
   array = []
   while i < a.length
-    array << yield(a[i])
+    array << yield(a[i]) + '!!!'
     i = i + 1
   end
   array
 end
+
+
+array.map do |element|
+  element.capitalized
+end
+
+my_collect do |element|
+  element.capitalized
+end
+
+self.map() {
+  array = []
+  array.push(yield)
+  array
+}
