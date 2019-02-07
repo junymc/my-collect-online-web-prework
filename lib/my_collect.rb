3ed3ba02c8b191985170d01a7d0110cce1cbb977
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-07 21:02:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,12 @@
 collection = ['ruby', 'javascript', 'python', 'objective-c']
-def my_collect(collection)
-my_collect(collection) do |lang|
+def my_collect(collection) 
   lang.upcase
 end
 
 def my_collect(array)
   i = 0
   while i < array.length
-    yield my_collect(collection)
+    yield 
 
     i = i + 1
   end
