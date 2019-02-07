2b7240e5397238abb5eca6037de3b481ee930ce0
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-07 21:16:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 
-def my_collect(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
-end
+# def my_collect(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+#   array
+# end
